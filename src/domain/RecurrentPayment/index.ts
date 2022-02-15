7e57f555e14d56d80051bbf2573b979849284395
Ítalo Guglielmini <italo.guglielmini@gmail.com>
2022-02-15 14:32:31 -0300

    feat: ajustes de pagamento recorrente e testes automatizados para Keys

--- src/domain/RecurrentPayment/index.ts
+++ src/domain/RecurrentPayment/index.ts
@@ -1,3 +1,5 @@
+export * from './recurrentCreate';
+export * from './recurrentModify';
 export * from './recurrentTransaction';
 export * from './recurrentPayment.model';
 export * from './recurrentPaymentConsult';
