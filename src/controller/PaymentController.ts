e154acfc260472818eef876588ed385174b046f9
Renan Garatini <renan.garatini@opah.com.br>
2022-02-21 16:15:02 -0300

    Find All Payments

--- src/controller/PaymentController.ts
+++ src/controller/PaymentController.ts
@@ -5,6 +5,7 @@ import service from '../service/index';
 import { UpdateByGatewayIdService } from '../service/UpdateByGatewayIdService';
 import { FindPaymentByGatewayIdService } from '../service/FindPaymentByGatewayIdService';
 import { CreatePaymentConfigService } from '../service/CreatePaymentConfigService';
+import { FindAllPaymentService } from '../service/FindAllPaymentService';
 
 export class PaymentController {
     private logger = debug('payment-api:PaymentController');
@@ -18,6 +19,7 @@ export class PaymentController {
     private updateByGatewayIdService = new UpdateByGatewayIdService();
     private findPaymentByGatewayIdService = new FindPaymentByGatewayIdService();
     private createPaymentConfigService = new CreatePaymentConfigService();
+    private findAllPaymentService = new FindAllPaymentService();
 
     public MakePayment = async (req: Request, res: Response) => {
         this.logger(`Creating payment`, req.body);
@@ -42,6 +44,19 @@ export class PaymentController {
                 : undefined,
             req.query.daysFilter ? Number(req.query.daysFilter) : undefined,
         );
+
+        if (data instanceof Error) {
+            this.logger('Error', data.message);
+            return res.status(422).json({ ['Error']: data.message });
+        }
+
+        return res.status(200).json(data);
+    };
+
+    public getAllPayments = async (_req: Request, res: Response) => {
+        this.logger(`getAllPayments`);
+
+        const data = await this.findAllPaymentService.execute();
 
         if (data instanceof Error) {
             this.logger('Error', data.message);
