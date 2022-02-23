180718f3a1dea31f969036677c76f9d8ef1e279d
elioglima <elio.designer@hotmail.com>
2022-02-23 18:24:48 -0300

    fix makePayment and adapter

--- src/service/CardAddService.ts
+++ src/service/CardAddService.ts
@@ -3,43 +3,39 @@ import { Cielo } from '../cielo';
 import { PaymentCards } from '../domain/Payment';
 import { PaymentCardsRepository } from '../dataProvider/repository/PaymentCardsRepository';
 import { reqCardAdd } from '../domain/IAdapter';
+import Adapter from '../domain/Adapter';
 
 export default class CardAddService {
     private logger = debug('service-api:CardAddService');
     private paymentCardRepository = new PaymentCardsRepository();
-    private cielo = new Cielo();
+    private paymentOperator = new Adapter()
 
     public execute = async (paymentCard: PaymentCards) => {
         this.logger(`Find Card Add`);
+        await this.paymentOperator.init(paymentCard.enterpriseId)
 
-        await this.cielo.init({ enterpriseId: paymentCard.enterpriseId });
+        const requestCardAdd: reqCardAdd = {
+            brand: paymentCard.brand,
+            cardNumber: paymentCard.cardNumber,
+            customerName: paymentCard.customerName,
+            expirationDate: paymentCard.expirationDate,
+            holder: paymentCard.holder,
+        };
 
-        const cardCielo = this.cielo.card;
+        const response: any = await this.paymentOperator.cardAdd(requestCardAdd)
 
-        if (cardCielo) {
-            const cardToken: reqCardAdd = {
-                brand: paymentCard.brand,
-                cardNumber: paymentCard.cardNumber,
-                customerName: paymentCard.customerName,
-                expirationDate: paymentCard.expirationDate,
-                holder: paymentCard.holder,
-            };
-            const token = await cardCielo
-                .createCardTokenized(cardToken)
-                .catch(e => new Error(e.Message));
-
-            if (token instanceof Error) {
-                console.log(token);
-                return token;
-            }
-
-            return await this.paymentCardRepository.persist({
-                ...paymentCard,
-                token: token.cardToken,
-                lastFourNumbers: paymentCard.cardNumber.slice(-4),
-            });
+        if (response?.error == true) {
+            return response
         }
 
-        return new Error('Cannot instance Card');
+        if (!response?.cardToken) {
+            return new Error('Cannot instance Card');
+        }
+
+        return await this.paymentCardRepository.persist({
+            ...paymentCard,
+            token: response.cardToken,
+            lastFourNumbers: paymentCard.cardNumber.slice(-4),
+        });
     };
 }
