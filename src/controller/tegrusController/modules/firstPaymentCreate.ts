7fdcaf8652d9b76d50413452a379666c2603afac
elioglima <elio.designer@hotmail.com>
2022-03-07 21:31:30 -0300

    fix payment

--- src/controller/tegrusController/modules/firstPaymentCreate.ts
+++ src/controller/tegrusController/modules/firstPaymentCreate.ts
@@ -4,11 +4,11 @@ import { TFirstPayment } from '../../../domain/Tegrus'
 
 /*
             
+    * criar uma tabela para pre cadastro usuarios - preRegisterResident
+    * criar uma tabela para faturas usuario nao logado - invoices    
     * criar uma tabela para armazenamento do token
     * gerar um link para o cliente acessar a tela de paramento
-    * criar uma tabela para pre cadastro
-    * criar uma tabela para faturas
-    * criar recibos de faturas pagas
+    * criar recibos de faturas pagas comprovantes - receipts
 
 */
 
@@ -20,6 +20,20 @@ type resFirstPaymentCreate = {
 const firstPaymentCreate = (req: Request, res: Response): (resFirstPaymentCreate | TErrorGeneric) => {
     try {
         const body: TFirstPayment = req?.body;
+
+        /* 
+            
+            1 - criar uma tabela para pre cadastro usuarios - preRegisterResident
+            2 - criar uma tabela para faturas usuario nao logado - invoices    
+            3 - criar recibos de faturas pagas comprovantes - receipts
+            
+            3 - createHash - gerar hash - criar date para tempo de validade
+            4 - criar uma tabela para armazenamento do token
+
+            5 - enviar o link para o cliente SMS e EMAIL
+                        
+        */
+
         return res.status(200).json(body);
     } catch (error: any) {
         return res.status(422).json(error);
