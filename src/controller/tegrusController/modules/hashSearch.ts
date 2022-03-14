537cb1f049d44487c03572697ae2b28e60290dec
Roberto Mota <robertomota@ext.cvccorp.com.br>
2022-03-14 17:13:40 -0300

    ajusts

--- src/controller/tegrusController/modules/hashSearch.ts
+++ src/controller/tegrusController/modules/hashSearch.ts
@@ -9,17 +9,37 @@ const hashSearch = async (req: Request, res: Response) => {
     try {
         const hash: string = req?.params?.hash;
         if (!hash)
-            return res.status(422).json({ ['Error']: 'None hash was sent.' });
+            return res.status(400).json({
+                err: true,
+                data: {
+                    message: 'No hash was sent.',
+                },
+            });
 
-        const data = await new tegrusServices.HashSearchService().execute(hash)
-
+        const data = await new tegrusServices.HashSearchService().execute(hash);
+        
         if (data instanceof Error) {
             logger('Error', data.message);
-            return res.status(422).json({ ['Error']: data.message });
+            return res.status(500).json({
+                err: true,
+                data: {
+                    message: data.message,
+                },
+            });
+        }
+
+        if (!data) {
+            logger('Error', 'Nothing was found');
+            return res.status(404).json({
+                err: true,
+                data: {
+                    message: 'Nothing was found',
+                },
+            });
         }
 
         return res.status(200).json(data);
-    } catch (error: any) {
+    } catch (error: any) {        
         return res.status(422).json(error);
     }
 };
