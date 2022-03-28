7bdb5e914ded39d360b0dad0c1ee943b92ea2174
elioglima <elio.designer@hotmail.com>
2022-03-28 09:37:48 -0300

    fix recurrence

--- src/controller/tegrusController/modules/hashSearch.ts
+++ src/controller/tegrusController/modules/hashSearch.ts
@@ -17,7 +17,6 @@ const hashSearch = async (req: Request, res: Response) => {
             });
 
         const data = await new tegrusServices.HashSearchService().execute(hash);
-    
 
         if (data instanceof Error) {
             logger('Error', data.message);
@@ -41,8 +40,11 @@ const hashSearch = async (req: Request, res: Response) => {
             });
         }
 
-        
-        return res.status(200).json({status: 200, data: data});
+        return res.status(200).json({
+            err: false,
+            status: 200,
+            data: data,
+        });
     } catch (error: any) {
         return res.status(422).json(error);
     }
