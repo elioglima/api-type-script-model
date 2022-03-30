f53e158642f08feb492f4a2f03c9f92a4a4baa93
elioglima <elio.designer@hotmail.com>
2022-03-30 19:29:29 -0300

    fix

--- src/controller/invoicesController/modules/getInvoices.ts
+++ src/controller/invoicesController/modules/getInvoices.ts
@@ -1,38 +1,31 @@
 import { Request, Response } from 'express';
-import InvoiceService from 'src/service/InvoiceService';
+import InvoiceService from '../../../service/invoiceService';
 import { TInvoiceFilter } from 'src/domain/Tegrus';
 
 export const getInvoices = async (req: Request, res: Response) => {
     try {
-        const { dateStart, dateEnd, residentId, idUser, invoiceId, paymentMethod, statusInvoice }: any = req?.query;
+        const {
+            startDate,
+            endDate,
+            residentId,
+            userId,
+            invoiceId,
+            paymentMethod,
+            statusInvoice,
+        }: any = req?.query;
 
-        // if (dateStart && dateEnd && (residentId || idUser))
-        //     return {
-        //         err: true,
-        //         data: {
-        //             message:
-        //                 'Please inform the filters(dateStart, dateEnd, residentId or idUser))',
-        //         },
-        //     };
-
-        const invoicesFilter:TInvoiceFilter = {
-            // dateStart,
-            // dateEnd,
-            ...residentId ? {residentId: residentId} : {},
-            ...idUser ? {idUser: idUser} : {},
-            ...invoiceId ? {invoiceId: invoiceId} : {},
-            ...paymentMethod ? {paymentMethod: paymentMethod} : {},
-            ...statusInvoice ? {statusInvoice: statusInvoice} : {}            
-        }
-        
-        console.log("invoicesFilter", invoicesFilter)
-
+        const invoicesFilter: TInvoiceFilter = {
+            startDate,
+            endDate,
+            residentId,
+            userId,
+            invoiceId,
+            paymentMethod,
+            statusInvoice,
+        };
 
         const invoiceService = new InvoiceService();
         const response: any = await invoiceService.Find(invoicesFilter);
-        
-        console.log("RESP", response)
-
         if (response.err) {
             return res.status(422).json(response);
         }
