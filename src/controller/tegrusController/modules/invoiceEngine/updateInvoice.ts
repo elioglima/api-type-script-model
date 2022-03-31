1dbe3522fe8cd641f03bf196e1a97165513e3145
Renan Garatini <renan.garatini@opah.com.br>
2022-03-31 16:08:15 -0300

    Update Invoice

--- src/controller/tegrusController/modules/invoiceEngine/updateInvoice.ts
+++ src/controller/tegrusController/modules/invoiceEngine/updateInvoice.ts
@@ -1,17 +1,29 @@
+import InvoiceService from "src/service/invoiceService";
+
 const updateInvoice = async (toReceive: any) => {
     try {
         // retorno do app >> bff >> tegrus
-        console.log(toReceive);
-        /* 
+        const updataData :any = {
+            value: toReceive?.updateInvoice?.value,
+            condominium: toReceive?.updateInvoice?.condominium,
+            discount: toReceive?.updateInvoice?.discount,
+            tax: toReceive?.updateInvoice?.tax,
+            refund: toReceive?.updateInvoice?.refund,
+            fine: toReceive?.updateInvoice?.fine,
+            fineTicket: toReceive?.updateInvoice?.fineTicket,
+            dueDate: toReceive?.updateInvoice?.dueDate,
+            description: toReceive?.updateInvoice?.description,
+            anticipation: toReceive?.updateInvoice?.anticipation,
+            type: toReceive?.updateInvoice?.type,
+            paymentMethod: toReceive?.updateInvoice?.paymentMethod,
+            statusInvoice: toReceive?.updateInvoice?.statusInvoice,
+            apartmentId: toReceive?.updateInvoice?.apartmentId,
+            invoiceId: toReceive?.updateInvoice?.invoiceId,
+        };
 
-            return {
-            err: true,
-            data: {
-                // resposta de erro
-            }
-            }
+        const invoiceService = new InvoiceService();
+        await invoiceService.Update(updataData);
 
-        */
         return {
             err: false,
             data: {
