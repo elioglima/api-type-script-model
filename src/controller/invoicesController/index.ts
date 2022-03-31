c721b573966d243e3f58bdaa18013d956a74636a
corpp00728 <robertomota@ext.cvccorp.com.br>
2022-03-30 22:30:25 -0300

    reorg and merge

--- src/controller/invoicesController/index.ts
+++ src/controller/invoicesController/index.ts
@@ -4,7 +4,7 @@ import * as md from "./modules/invoicesFilter"
 
 export class InvoicesController {
     
-    public getInvoices = async (req: Request, res: Response) => {
+    public invoicesFilter = async (req: Request, res: Response) => {
         return await md.invoicesFilter(req, res)
     };
 
