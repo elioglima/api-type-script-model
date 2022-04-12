40e05006a96e9ebaec83b97b2199443011f45cad
elioglima <elio.designer@hotmail.com>
2022-04-12 09:29:21 -0300

    fix find hash

--- src/config/webserver/routes/tegrus.routes.ts
+++ src/config/webserver/routes/tegrus.routes.ts
@@ -8,5 +8,5 @@ tegrus
     .post('/topic/invoiceEngine', PaymentTegrusController.invoiceEngine)
     .get('/topic/invoiceEngine', PaymentTegrusController.invoiceEnginePrivate)
     .get('/gateway/:hash', PaymentTegrusController.hashSearch)
-    .post('/gateway/paynow', PaymentTegrusController.gatewayPaynow)
-    .get('/pay/config', PaymentTegrusController.payConfig);
+    .post('/gateway/paynow', PaymentTegrusController.gatewayPaynow);
+// .get('/pay/config', PaymentTegrusController.payConfig);
