e001ea138b54176df4b82fb359b0435cae030acb
elioglima <elio.designer@hotmail.com>
2022-04-20 13:21:30 -0300

    yarn tsc

--- src/config/webserver/routes/tegrus.routes.ts
+++ src/config/webserver/routes/tegrus.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { TegrusController } from '../../../controller/tegrusController';
-import {invoiceEnginePrivate} from '../../../controller/tegrusController/modules/invoiceEnginePrivate/'
+// import {invoiceEnginePrivate} from '../../../controller/tegrusController/modules/invoiceEnginePrivate/'
 
 const PaymentTegrusController = new TegrusController();
 export const tegrus: Router = Router();
@@ -9,7 +9,4 @@ tegrus
     .post('/topic/invoiceEngine', PaymentTegrusController.invoiceEngine)
     .get('/topic/invoiceEngine', PaymentTegrusController.invoiceEnginePrivate)
     .get('/gateway/:hash', PaymentTegrusController.hashSearch)
-    .post('/gateway/paynow', PaymentTegrusController.gatewayPaynow)
-    
-    
-
+    .post('/gateway/paynow', PaymentTegrusController.gatewayPaynow);
