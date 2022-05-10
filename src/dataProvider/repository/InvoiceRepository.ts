10a5a7ba84e9fe92660814915d75a97c5876ceee
elioglima <elio.designer@hotmail.com>
2022-05-10 16:35:32 -0300

    fix

--- src/dataProvider/repository/InvoiceRepository.ts
+++ src/dataProvider/repository/InvoiceRepository.ts
@@ -129,7 +129,7 @@ export class InvoiceRepository {
             });
         }
 
-        return await db.orderBy('invoice.atUpdate', 'DESC').getMany();
+        return await db.orderBy('invoice.updateDate', 'DESC').getMany();
     };
 
     public update = async (invoice: TInvoice) => {
